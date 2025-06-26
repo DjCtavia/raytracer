4a3eb6fe704e07c79a18a6718dcb7c76715b081c
DjCtavia <djctavia@gmail.com>
2025-06-27 00:00:59 +0200

    feat: improve sphere hit

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@
  * @param r
  * @return
  */
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     vec3 originSphere = r.origin() - center; // O = A - C
     auto a = dot(r.direction(), r.direction()); // O ⋅ O
@@ -43,13 +43,19 @@ bool hit_sphere(const point3& center, double radius, const ray& r)
     auto c = dot(originSphere, originSphere) - radius * radius; // O ⋅ O - r²
     auto discriminant = b * b - 4 * a * c; // at² + bt + c = 0
 
-    return discriminant >= 0;
+    return discriminant < 0
+               ? -1.0
+               : (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 color ray_color(const ray& r)
 {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return {1, 0, 0};
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
